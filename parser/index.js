58b21c00285b437e1ad48221cad7750b32fef300
xiedacon <xiedacon@qq.com>
2017-07-23 15:25:12 +0800

    refact(parser): refact

--- parser/index.js
+++ parser/index.js
@@ -1,7 +1,6 @@
 'use strict'
 
 const cheerio = require('cheerio')
-const logger = require('../logger.js')
 
 module.exports = {
   getRepos: (text) => {
@@ -11,39 +10,32 @@ module.exports = {
     return repos.toArray().map((ele) => {
       ele = $(ele)
 
-      try {
-        let [author, repoName] = /<span class="text-normal">(.*) \/ <\/span>(.*)/g.exec($(ele.find('a')[0]).html())
-          .slice(1, 3)
-          .map((str) => {
-            return str.trim()
-          })
-        let content = (ele.find('p').html() || '').trim()
-        let [programmingLanguage, starTotal, fork, starIncrease] = ((ele) => {
-          return [
-            (ele.find('span[itemprop="programmingLanguage"]').html() || '').trim(),
-            ele.find(`a[href="/${author}/${repoName}/stargazers"]`).text().trim(),
-            ele.find(`a[href="/${author}/${repoName}/network"]`).text().trim(),
-            (/^(\d+).*/g.exec((ele.find('span[class="float-right"]').text() || '').trim()) || [])[1]
-          ]
-        })(ele.find('div.f6.text-gray.mt-2'))
+      let [author, repoName] = (/<span class="text-normal">(.*) \/ <\/span>(.*)/g.exec($(ele.find('a')[0]).html()) || [])
+        .slice(1, 3)
+      let content = ele.find('p').html() || ''
+      let [programmingLanguage, starTotal, fork, starIncrease] = ((ele) => {
+        return [
+          ele.find('span[itemprop="programmingLanguage"]').html() || '',
+          ele.find(`a[href="/${author}/${repoName}/stargazers"]`).text() || '',
+          ele.find(`a[href="/${author}/${repoName}/network"]`).text() || '',
+          (/(\d+) stars today/g.exec((ele.text() || '').trim()) || ['', '0'])[1]
+        ]
+      })(ele.find('div.f6.text-gray.mt-2'))
 
-        return {
-          author: author,
-          repoName: repoName,
-          content: content,
-          programmingLanguage: programmingLanguage,
-          starTotal: starTotal,
-          fork: fork,
-          starIncrease: starIncrease
-        }
-      } catch (error) {
-        logger.error('ParseError: ' + error.stack)
+      return {
+        author: author.trim(),
+        repoName: repoName.trim(),
+        content: content.trim(),
+        programmingLanguage: programmingLanguage.trim(),
+        starTotal: starTotal.trim(),
+        fork: fork.trim(),
+        starIncrease: starIncrease.trim()
       }
     })
   },
   getLanguages: (text) => {
     let $ = cheerio.load(text)
-    let languages = $('ul.filter-list.small.language-filter-list a')
+    let languages = $('ul.filter-list.small a')
 
     return languages.toArray().map((language) => {
       return {
